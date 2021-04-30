c531cdf3eacd5f517d0085726771767b8c887a29
Marek Wadinger <50716630+MarekWadinger@users.noreply.github.com>
2021-04-30 18:54:10 +0200

    Update surpress_output.py

--- src/loadforecast/surpress_output.py
+++ src/loadforecast/surpress_output.py
@@ -4,12 +4,12 @@ import os
 class suppress_printout(object):
     """Suppress print out upstream
     A context manager for doing a "deep suppression" of stdout and stderr in
-    Python, i.e. will suppress all print, even if the print originates in a
-    compiled C/Fortran sub-function.
+    Python, i.e. will suppress all print, even if the print originates from a
+    compiled.
 
        This will not suppress raised exceptions, since exceptions are printed
     to stderr just before a script exits, and after the context manager has
-    exited (at least, I think that is why it lets exceptions through).
+    exited.
     """
     def __init__(self):
         # Open a pair of null files
